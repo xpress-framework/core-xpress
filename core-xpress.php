1f0f6180c0ac4579c1675f5ea493e0fa678f5331
Juan Manuel Arias <trasgofurioso@gmail.com>
2016-06-18 20:15:04 -0300

    Registerin child plugins

--- core-xpress.php
+++ core-xpress.php
@@ -7,16 +7,46 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
 
 if ( ! class_exists( 'Core_Xpress' ) ) {
 	class Core_Xpress {
+		/**
+		 * Plugin Slug
+		 * @var string
+		 */
 		public static $plugin_slug = 'core-xpress';
 
+		/**
+		 * Required directories
+		 * @var array
+		 */
+		private $required_directories;
+
 		public function __construct() {
-			add_action( 'plugins_loaded', array( $this, 'requires' ), 0 );
+			$this->required_directories = array(
+				trailingslashit( plugin_dir_path( __FILE__ ) . 'includes' )
+			);
+			add_action( 'plugins_loaded', array( $this, 'register_child_plugins' ), 0 );
+			add_action( 'plugins_loaded', array( $this, 'requires' ), 5 );
 		}
 
+		public function register_child_plugins() {
+			$plugins = apply_filters( self::$plugin_slug . '/child-plugins', array() );
+			foreach ( $plugins as $plugin ) {
+				$this->required_directories = array_merge( $this->required_directories, $this->require_child_plugin_directories( $plugin ) );
+			}
+		}
+
+		public function require_child_plugin_directories( $plugin ) {
+			return array(
+				trailingslashit( $plugin['path'] . 'post-types' ),
+				trailingslashit( $plugin['path'] . 'taxonomies' ),
+				trailingslashit( $plugin['path'] . 'custom-fields' ),
+				trailingslashit( $plugin['path'] . 'controllers' ),
+				trailingslashit( $plugin['path'] . 'components' ),
+				trailingslashit( $plugin['path'] . 'functions' ),
+				trailingslashit( $plugin['path'] . 'admin' ),
+			);
+		}
 		public function requires() {
-			$directories = apply_filters( self::$plugin_slug . '/requires', array(
-				trailingslashit( plugin_dir_path( __FILE__ ) . 'includes' )
-			) );
+			$directories = apply_filters( self::$plugin_slug . '/required_directories', $this->required_directories );
 			foreach ( $directories as $directory ) {
 				$this->require_dir( $directory );
 			}
